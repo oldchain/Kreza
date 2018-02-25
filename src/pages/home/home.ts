a3d1cad7fd64daa7c09f5cd49ac5b353816a14c9
Oldchain <hussein.adel29@yahoo.com>
2018-02-25 16:48:07 +0200

    Basic App Structure

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  searchQuery: string = '';
+  items: string[];
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota'
+    ];
+  }
   constructor(public navCtrl: NavController) {
+    this.initializeItems();
+  }
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
 
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
